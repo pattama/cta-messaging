5ef842745e17b52d5e4b7b2ef551dd1a0f9bb22c
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-07-23 16:10:13 +0200

    updated info sample

--- samples/basic/info.js
+++ samples/basic/info.js
@@ -1,16 +1,8 @@
 'use strict';
 
-// subscribe to receive messages
-const IoLib = require('../../lib');
-
-const provider = process.argv.slice(2).join() || 'rabbitmq';
-console.log('Using provider "' + provider + '"');
-
-const io = new IoLib(provider);
-
-io.info({
-  queue: 'test',
-}).then(function(response) {
+const messaging = require('cta-messaging')();
+messaging.info('test')
+.then(function(response) {
   console.log('response: ', response);
 }, function(err) {
   console.error('error: ', err);
