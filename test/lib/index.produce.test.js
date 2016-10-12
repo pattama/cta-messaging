fa1a84bf685de276ce7478dc7ee9a9c960a3759b
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-10-12 17:05:17 +0200

    added expires parameter for produce method fixed info method

---
+++ test/lib/index.produce.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const o = require('../common');
+
+describe('produce', function() {
+  this.timeout(5000);
+  const io = o.lib();
+  it('expires', function(done) {
+    const queue = o.shortid.generate();
+    return o.co(function* coroutine() {
+      const produce = yield io.produce({
+        queue: queue,
+        json: {foo: 'bar'},
+        autoDelete: true,
+        expires: 1000,
+      });
+      console.log('> produce: ', produce);
+      const info1 = yield io.info(queue);
+      console.log('> info1: ', info1);
+      yield o.sleep(2000);
+      const info2 = yield io.info(queue);
+      console.log('> info2: ', info2);
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+  });
+});
