8e8145bc8fe0ea903a7e66280e186a0164ddc78e
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-01-04 12:15:07 +0100

    house keeping coverage

---
+++ test/lib/providers/rabbitmq/main/house-keeping.test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const o = require('../../../../common');
+
+describe('house keeping', function() {
+  it('should clear not acked messages from memory after clearOffset delay', function(done) {
+    o.co(function *() {
+      const content = o.json();
+      const queue = o.queue();
+      const mq = new o.lib({}, {
+        properties: {
+          provider: 'rabbitmq',
+          parameters: {
+            clearInterval: 500,
+            clearOffset: 100,
+          },
+        },
+        singleton: false,
+      });
+      yield mq.produce({
+        queue: queue,
+        content: content,
+      });
+      yield mq.get({
+        queue: queue,
+      });
+      o.assert.strictEqual(Object.keys(mq.messages).length, 1);
+      o.assert.property(mq.messages, content.id);
+      setTimeout(function() {
+        o.assert.strictEqual(Object.keys(mq.messages).length, 0);
+        done();
+      }, 500);
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('should clear acked messages from memory after clearOffset delay', function(done) {
+    o.co(function *() {
+      const content = o.json();
+      const queue = o.queue();
+      const mq = new o.lib({}, {
+        properties: {
+          provider: 'rabbitmq',
+          parameters: {
+            clearInterval: 500,
+            clearOffset: 100,
+          },
+        },
+        singleton: false,
+      });
+      yield mq.produce({
+        queue: queue,
+        content: content,
+      });
+      yield mq.get({
+        queue: queue,
+      });
+      o.assert.strictEqual(Object.keys(mq.messages).length, 1);
+      o.assert.property(mq.messages, content.id);
+      yield mq.ack(content.id);
+      setTimeout(function() {
+        o.assert.strictEqual(Object.keys(mq.messages).length, 0);
+        o.assert.strictEqual(Object.keys(mq.acked).length, 0);
+        done();
+      }, 500);
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+});
