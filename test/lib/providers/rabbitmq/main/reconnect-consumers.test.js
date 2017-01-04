7b524f18c2d07a6f763471e512084653247ff5e1
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-01-04 13:19:56 +0100

    reconnect consumers coverage

---
+++ test/lib/providers/rabbitmq/main/reconnect-consumers.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const o = require('../../../../common');
+
+describe('reconnect consumers', function() {
+  it('should reconnect all known consumers after reconnection', function(done) {
+    o.co(function *() {
+      const queue = o.queue();
+      const mq = o.mq();
+      mq.config.reConnectAfter = 500;
+      const consumer = yield mq.consume({
+        queue: queue,
+        cb: () => {},
+      });
+      o.assert.property(mq.consumers, consumer.result.consumerTag);
+      const _consume = o.sinon.spy(mq, 'consume');
+      mq.connection.emit('close');
+      setTimeout(function() {
+        o.assert.strictEqual(_consume.callCount, 1);
+        o.assert.notProperty(mq.consumers, consumer.result.consumerTag);
+        o.assert(Object.keys(mq.consumers).length);
+        mq.consume.restore();
+        done();
+      }, 1000);
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+
+});
