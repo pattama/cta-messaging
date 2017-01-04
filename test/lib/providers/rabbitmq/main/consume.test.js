bb5a9c289b366265ad8f6a08a69feeb8e26db32f
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-01-04 16:35:31 +0100

    consume coverage

--- test/lib/providers/rabbitmq/main/consume.test.js
+++ test/lib/providers/rabbitmq/main/consume.test.js
@@ -2,27 +2,27 @@
 
 const o = require('../../../../common');
 
-describe('subscribe', function() {
+describe('consume', function() {
 
   it('should ack when option is set to resolve', function(done) {
     const mq = o.mq();
     o.co(function *() {
-      const topic = o.topic();
+      const queue = o.queue();
       const content = o.json();
       yield mq._connect(false);
       const channel = yield mq._channel();
       o.sinon.stub(channel, 'ack');
       o.sinon.stub(mq, '_channel', () => { return Promise.resolve(channel); });
-      yield mq.subscribe({
-        topic: topic,
+      yield mq.consume({
+        queue: queue,
         cb: () => {
           return Promise.resolve();
         },
         ack: 'resolve',
       });
       mq._channel.restore();
-      yield mq.publish({
-        topic: topic,
+      yield mq.produce({
+        queue: queue,
         content: content,
       });
       setTimeout(() => {
@@ -38,12 +38,12 @@ describe('subscribe', function() {
   it('should reject when create channel fails', function(done) {
     const mq = o.mq();
     o.co(function *() {
-      const topic = o.topic();
+      const queue = o.queue();
       o.sinon.stub(mq, '_channel', () => {
         return Promise.reject('some_error');
       });
-      yield mq.subscribe({
-        topic: topic,
+      yield mq.consume({
+        queue: queue,
         cb: () => {},
       });
       done('should not be here');
@@ -57,7 +57,7 @@ describe('subscribe', function() {
 
   it('should not call cb when consumed message is null', function(done) {
     o.co(function *() {
-      const topic = o.topic();
+      const queue = o.queue();
       const mq = o.mq();
       yield mq._connect(true);
       const channel = yield mq._channel();
@@ -69,8 +69,8 @@ describe('subscribe', function() {
         return Promise.resolve(channel);
       });
       const cb = o.sinon.stub();
-      yield mq.subscribe({
-        topic: topic,
+      yield mq.consume({
+        queue: queue,
         cb: cb,
       });
       o.assert.strictEqual(cb.callCount, 0);
@@ -84,16 +84,16 @@ describe('subscribe', function() {
 
   it('should not call cb when processed message is null', function(done) {
     o.co(function *() {
-      const topic = o.topic();
+      const queue = o.queue();
       const mq = o.mq();
       const cb = o.sinon.stub();
-      yield mq.subscribe({
-        topic: topic,
+      yield mq.consume({
+        queue: queue,
         cb: cb,
       });
       o.sinon.stub(mq, '_bufferToJSON').returns(null);
-      yield mq.publish({
-        topic: topic,
+      yield mq.produce({
+        queue: queue,
         content: o.json(),
       });
       setTimeout(function() {
@@ -109,18 +109,18 @@ describe('subscribe', function() {
 
   it('should log cb error when rejected', function(done) {
     o.co(function *() {
-      const topic = o.topic();
+      const queue = o.queue();
       const mq = o.mq();
       const cb = () => {
         return Promise.reject('cb_error');
       };
-      yield mq.subscribe({
-        topic: topic,
+      yield mq.consume({
+        queue: queue,
         cb: cb,
       });
       o.sinon.stub(mq.logger, 'error');
-      yield mq.publish({
-        topic: topic,
+      yield mq.produce({
+        queue: queue,
         content: o.json(),
       });
       setTimeout(() => {
@@ -138,18 +138,18 @@ describe('subscribe', function() {
 
   it('should log cb error when thrown', function(done) {
     o.co(function *() {
-      const topic = o.topic();
+      const queue = o.queue();
       const mq = o.mq();
       const cb = () => {
         throw new Error('cb_error');
       };
-      yield mq.subscribe({
-        topic: topic,
+      yield mq.consume({
+        queue: queue,
         cb: cb,
       });
       o.sinon.stub(mq.logger, 'error');
-      yield mq.publish({
-        topic: topic,
+      yield mq.produce({
+        queue: queue,
         content: o.json(),
       });
       setTimeout(() => {
