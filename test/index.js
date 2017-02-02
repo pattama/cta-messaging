a894fac5b24092fdd971c6e6c0c0e38714e70ad6
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-02-02 10:52:32 +0100

    updated gitlabci

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const amqp = require('amqplib');
 
 describe('RabbitMQ Gitlab CI Docker tests', function() {
   it('rabbitmq connection', function(done){
-    amqp.connect('amqp://myhost?heartbeat=60')
+    amqp.connect('amqp://rabbitmq?heartbeat=60')
       .then(() => {
         done();
       })
