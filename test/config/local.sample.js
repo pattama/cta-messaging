3f0e9ade3321e5728ee80d7892b1be9ee84138c1
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-02-02 12:22:25 +0100

    added support of local environment variables

---
+++ test/config/local.sample.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Rename this file to local.js and override in your custom parameters
+ */
+
+module.exports = {
+  rabbitMqUrl: 'amqp://localhost?heartbeat=60',
+};
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const amqp = require('amqplib');
+const config = require('./config');
 
 describe('RabbitMQ Gitlab CI Docker tests', function() {
   it('rabbitmq connection', function(done){
-    amqp.connect('amqp://rabbitmq?heartbeat=60')
+    amqp.connect(config.rabbitMqUrl)
       .then(() => {
         done();
       })
