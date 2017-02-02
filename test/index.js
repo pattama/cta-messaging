9291c1004b0e166e8122858780c292da65d18438
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2017-02-02 13:23:47 +0100

    updated test

--- test/index.js
+++ test/index.js
@@ -5,6 +5,8 @@ const config = require('./config');
 
 describe('RabbitMQ Gitlab CI Docker tests', function() {
   it('rabbitmq connection', function(done){
+    this.timeout(5000);
+    console.log(`trying connection to host '${config.rabbitMqUrl}'`);
     amqp.connect(config.rabbitMqUrl)
       .then(() => {
         done();
